94f5784b680256354685233b01a7dad0128f2862
Chris <chris.otto6@gmail.com>
2017-11-21 19:13:30 -0600

    Resolves #13 Using the edit callback I was able to return the formatted wiki

--- src/extension.ts
+++ src/extension.ts
@@ -3,66 +3,47 @@
 import * as vscode from 'vscode';
 import { WikiFormatter } from './formatter';
 
-// called when your extension is activated
+// called when the extension is activated
 export function activate(ctx: vscode.ExtensionContext) {
     console.log('FitNesse Formatter is up and running.');
 
     // Create a new instance of the formatter class for command execution and for controller on save functionality
-    let commandExec = new CommandFormat();
     let instance = new Formatter();
     let controller = new FormatterController(instance);
 
-    // Being that we want to be able to execute the format from keybinding and on save we have a few things going on
-    // We create the disposable for the command registration, after that we handle the create through the controller
-    // which is subscribed to the on save event from the workspace
-    let disposable = vscode.commands.registerCommand('extension.format', () => {
-        
-        commandExec.format();
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hey there.');
-    });
-
-    ctx.subscriptions.push(disposable);
     ctx.subscriptions.push(controller);
-    ctx.subscriptions.push(commandExec);
 }
 
 class Formatter {
-    private needsFormat = false;
-
     // Main format function which will happen on save or when the keymapping is pressed
     public format() {
+
+        // Variable creation
         let _editor = vscode.window.activeTextEditor;
         let _doc = _editor.document;
+        let lastLine = _doc.lineAt(_doc.lineCount - 2);
+        let start = new vscode.Position(0, 0);
+        let end = new vscode.Position(_doc.lineCount - 1, lastLine.text.length);
 
         if (!_editor) {
           return;
-        } 
-
-        console.log(_doc.languageId);
+        }
 
         // Need to make sure the document is a text document see language-configuration.json and package.json
         // For specifics around the FitNesse language addition
-        if (_doc.languageId === "text") {
-            let jarjar = new WikiFormatter();
+        if (_doc.languageId === "text" && _doc.isDirty) {
 
+            // Variable Creation
+            let wiki = new WikiFormatter();
             let text = _doc.getText();
-            console.log("formatting text: " + text.length);
-            let binks = jarjar.format(text);
+            let formattedText = wiki.format(text);
 
+            _editor.edit(builder => {
+                builder.replace(new vscode.Range(start, end), formattedText);
+            });
+            
             return;
         }
-    }
-
-    public setNeedsFormat(bool) {
-        this.needsFormat = bool;
-    }
-}
-
-class CommandFormat extends Formatter {
-    dispose() {
-        this.dispose();
     }
 }
 
@@ -76,7 +57,6 @@ class FormatterController {
         // subscribe to events
         let subsciptions: vscode.Disposable[] = [];
         vscode.workspace.onWillSaveTextDocument(this._onEvent, this, subsciptions);
-        vscode.workspace.onDidChangeTextDocument(this._isDirty, this, subsciptions);
 
         // create combined disposable for the event subsciptions
         this._disposable = vscode.Disposable.from(...subsciptions);
@@ -88,9 +68,5 @@ class FormatterController {
 
     private _onEvent() {
         this._formatter.format();
-    }
-
-    private _isDirty() {
-        this._formatter.setNeedsFormat(true);
     }
 }
