d62c54e4ea8cf48931214427c3a5fae654ae3df4
almightywiz <charles.lukas@gmail.com>
2017-11-30 10:32:11 -0600

    update context menu formatting to include a warning with file count to be formatted - update also includes standard formatting for a file that is in the current active editor when attempting to format via workspace context menu

--- src/format.ts
+++ src/format.ts
@@ -38,36 +38,48 @@ export class Formatter {
     
     public formatContextMenu(uri) {
 
+        const me = this; // because I'm lazy and don't want to deal with scope binding...
         const fs = require('fs');
         const path = require('path');
 
         // get all files in a given directory, or return an array containing a single file if URI is not a directory
         // https://gist.github.com/kethinov/6658166
-        const walkSync = (d) => fs.statSync(d).isDirectory() ? Array.prototype.concat(...fs.readdirSync(d).map(f => walkSync(path.join(d, f)))) : d;
+        const walkSync = (d) => fs.statSync(d).isDirectory() ? Array.prototype.concat(...fs.readdirSync(d).map(f => walkSync(path.join(d, f)))) : [d];
 
         let files = walkSync(uri);
         
-        files.forEach(file => {
-            if (!file.endsWith("content.txt")) {
+        files = files.filter(file => file.endsWith("content.txt"));
+        
+        vscode.window.showWarningMessage("You are about to format " + files.length + " test files.\nDo you wish to continue?", { modal: true }, "Yes", "No").then(answer => {
+            if (answer !== "Yes") {
                 return;
             }
-            
-            fs.readFile(file, "utf8", function(err, data) {
-                if(err) {
-                    return console.log(err);
+
+            let _editor = vscode.window.activeTextEditor;
+            files.forEach(file => {
+                if (_editor && _editor.document.fileName === file) {
+                    me.format();
+                    return;
                 }
-                
-                let wiki = new WikiFormatter();
-                let formattedText = wiki.format(data);
-                
-                fs.writeFile(file, formattedText, function(err) {
+
+                fs.readFile(file, "utf8", function(err, data) {
                     if(err) {
                         return console.log(err);
                     }
+                    
+                    let wiki = new WikiFormatter();
+                    let formattedText = wiki.format(data);
+                    
+                    fs.writeFile(file, formattedText, "utf8", function(err) {
+                        if(err) {
+                            return console.log(err);
+                        }
 
-                    console.log("The file was saved!");
+                        console.log("The file was saved!");
+                    });
                 });
             });
         });
+    
     }
 }
