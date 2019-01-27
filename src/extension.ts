eab7723f412ec233108d73e501ca2ca5e30d0df3
Chris Otto <chris.otto6@gmail.com>
2019-01-27 14:01:38 -0600

    Resolves #54 adding folding of fitnesse collapsable sections

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,43 @@
-'use strict';
+"use strict";
 
-import * as vscode from 'vscode';
-import { Formatter } from './format';
-import { FormatterController } from './formatController';
-import { TestRunner } from './runTest';
+import * as vscode from "vscode";
+import { Formatter } from "./format";
+import { FormatterController } from "./formatController";
+import { TestRunner } from "./runTest";
+import { activate as activateFitNesseFoldingProvider } from "./folding";
 
 // called when the extension is activated
 export function activate(ctx: vscode.ExtensionContext) {
-    console.log('FitNesse Formatter is up and running.');
+  console.log("FitNesse Formatter is up and running.");
 
-    // Create a new instance of the formatter class for command execution and for controller on save functionality
-    let instance = new Formatter();
-    let runner = new TestRunner();
-    let controller = new FormatterController(instance);
+  // Create a new instance of the formatter class for command execution and for controller on save functionality
+  let instance = new Formatter();
+  let runner = new TestRunner();
+  let controller = new FormatterController(instance);
+  const subscriptions: vscode.Disposable[] = [];
 
-    // Subscribe the Format Controller for on save functionality
-    ctx.subscriptions.push(controller);
-    
-    // Register the two different format commands for non on save functionality
-    ctx.subscriptions.push(vscode.commands.registerCommand('fitnesse.format', () => {
-        instance.format();
-    }));
-    ctx.subscriptions.push(vscode.commands.registerCommand('fitnesse.formatContextMenu', (uri) => {
-        instance.formatContextMenu(uri.fsPath);
-    }));
+  // Register the folding provider for FitNesse Collapsable sections
+  activateFitNesseFoldingProvider(ctx);
 
-    // Register the commands for the test runner functionality
-    ctx.subscriptions.push(vscode.commands.registerCommand('fitnesse.runTest', (uri) => {
-        runner.run();
-    }));
+  // Subscribe the Format Controller for on save functionality
+  ctx.subscriptions.push(controller);
+
+  // Register the two different format commands for non on save functionality
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand("fitnesse.format", () => {
+      instance.format();
+    })
+  );
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand("fitnesse.formatContextMenu", uri => {
+      instance.formatContextMenu(uri.fsPath);
+    })
+  );
+
+  // Register the commands for the test runner functionality
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand("fitnesse.runTest", uri => {
+      runner.run();
+    })
+  );
 }
